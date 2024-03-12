4e8e99431c62a6f3e394017424c498fb10a53338
Tiago Rezende <tiagosr@gmail.com>
2024-03-12 20:38:24 +0100

    Rendering all cardinal lines with a single mesh

--- Scripts/BaseScene.gd
+++ Scripts/BaseScene.gd
@@ -1,22 +1,38 @@
 extends Node3D
 
-var cardinalLineX = MeshInstance3D.new()
-var cardinalLineY: ImmediateMesh = ImmediateMesh.new()
-var cardinalLineZ: ImmediateMesh = ImmediateMesh.new()
+var cardinalLines = MeshInstance3D.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var cardinalLineXMesh = ImmediateMesh.new()
-	var cardinalLineMaterial = ORMMaterial3D.new()
-	cardinalLineXMesh.surface_begin(Mesh.PRIMITIVE_LINES, cardinalLineMaterial)
-	cardinalLineXMesh.surface_add_vertex(Vector3(-10000, 0, 0))
-	cardinalLineXMesh.surface_add_vertex(Vector3(10000, 0, 0))
-	cardinalLineXMesh.surface_end()
-	cardinalLineX.mesh = cardinalLineXMesh
-	cardinalLineX.cast_shadow = false
-	cardinalLineMaterial.albedo_color = Color(1.0, 0.0, 0.0)
-	cardinalLineMaterial.shading_mode = BaseMaterial3D.SHADING_MODE_UNSHADED
-	add_child(cardinalLineX)
+	var cardinalLinesMesh = ImmediateMesh.new()
+	var cardinalLineXMaterial = ORMMaterial3D.new()
+	var cardinalLineYMaterial = ORMMaterial3D.new()
+	var cardinalLineZMaterial = ORMMaterial3D.new()
+	cardinalLineXMaterial.albedo_color = Color(1.0, 0.0, 0.0)
+	cardinalLineXMaterial.shading_mode = BaseMaterial3D.SHADING_MODE_UNSHADED
+	cardinalLineYMaterial.albedo_color = Color(0.0, 1.0, 0.0)
+	cardinalLineYMaterial.shading_mode = BaseMaterial3D.SHADING_MODE_UNSHADED
+	cardinalLineZMaterial.albedo_color = Color(0.0, 0.0, 1.0)
+	cardinalLineZMaterial.shading_mode = BaseMaterial3D.SHADING_MODE_UNSHADED
+
+	cardinalLinesMesh.surface_begin(Mesh.PRIMITIVE_LINES, cardinalLineXMaterial)
+	cardinalLinesMesh.surface_add_vertex(Vector3(-10000, 0, 0))
+	cardinalLinesMesh.surface_add_vertex(Vector3( 10000, 0, 0))
+	cardinalLinesMesh.surface_end()
+
+	cardinalLinesMesh.surface_begin(Mesh.PRIMITIVE_LINES, cardinalLineYMaterial)
+	cardinalLinesMesh.surface_add_vertex(Vector3(0, -10000, 0))
+	cardinalLinesMesh.surface_add_vertex(Vector3(0,  10000, 0))
+	cardinalLinesMesh.surface_end()
+
+	cardinalLinesMesh.surface_begin(Mesh.PRIMITIVE_LINES, cardinalLineZMaterial)
+	cardinalLinesMesh.surface_add_vertex(Vector3(0, 0, -10000))
+	cardinalLinesMesh.surface_add_vertex(Vector3(0, 0,  10000))
+	cardinalLinesMesh.surface_end()
+
+	cardinalLines.mesh = cardinalLinesMesh
+	cardinalLines.cast_shadow = false
+	add_child(cardinalLines)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
